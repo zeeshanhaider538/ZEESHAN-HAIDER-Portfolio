eec92aabecd36bd20efe0376e7e3797c91ced71c
Zeeshan Haider <zeeshanhaider538@gmail.com>
2023-04-08 10:39:19 +0500

    Navbar Completed

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,20 @@
+ import React from 'react'
+ 
+ const Intro = () => {
+   return (
+     <div className="intro">
+        <div className="i-left">
+            <div className="i-name">
+                <span>Hy! I Am</span>
+                <span>Zeeshan Haider</span>
+                <span>MERN Stack Developer with high level of experience in web designing and development , producting the Quality work. 
+                </span>
+            </div>
+        
+        </div>
+        <div className="i-right"></div>
+     </div>
+   )
+ }
+ 
+ export default Intro
