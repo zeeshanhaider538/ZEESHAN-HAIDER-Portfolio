62d0e0d4a115f96718b22d4e02c6637871430198
Zeeshan Haider <zeeshanhaider538@gmail.com>
2023-04-09 10:58:23 +0500

    Intro Right Side

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -3,6 +3,12 @@ import "./Intro.css";
 import Github from "../../img/github.png";
 import LinkedIn from "../../img/linkedin.png";
 import Instagram from "../../img/instagram.png";
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import Boy from "../../img/boy.png";
+import Thumbup from "../../img/thumbup.png";
+import Crown from "../../img/crown.png";
+import Glassesimoji from "../../img/glassesimoji.png";
 const Intro = () => {
   return (
     <div className="intro">
@@ -26,7 +32,11 @@ const Intro = () => {
             <img src={Instagram} alt="Instagram"></img>
         </div>
       </div>
-      <div className="i-right">I am right side </div>
+      <div className="i-right">
+        <img src={Vector1} alt="Vector1"></img>
+        <img src={Vector2} alt="Vector2"></img>
+        <img src={Boy} alt="Boy"></img>
+         </div>
     </div>
   );
 };
