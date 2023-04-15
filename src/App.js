7a88e4c53cdb8c2784084b5274f3be29dd4b5604
Zeeshan Haider <zeeshanhaider538@gmail.com>
2023-04-15 15:46:23 +0500

    Experience section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Intro from './Components/Intro/Intro';
-import Services from './Components/Services/Services';
-
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Intro from "./Components/Intro/Intro";
+import Services from "./Components/Services/Services";
+import Experience from "./Components/Experience/Experience";
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <Intro/>
-     <Services/> 
+      <Navbar />
+      <Intro />
+      <Services />
+      <Experience />
     </div>
   );
 }
--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,12 +1,24 @@
-import React from "react";
-
+import React from 'react'
+import "./Experience.css";
 const Experience = () => {
-  <div className="experience">
+  return (
+    <div className="experience">
     <div className="achievement">
-      <div className="circle">1+</div>
+      <div className="circle">2+</div>
       <span>years</span>
+      <span>Experience</span>
     </div>
-  </div>;
-};
+    <div className="achievement">
+      <div className="circle">15+</div>
+      <span>completed</span>
+      <span>Projects</span>
+    </div>
+    <div className="achievement">
+      <div className="circle">2+</div>
+      <span>companies</span>
+    </div>
+  </div>
+  )
+}
 
-export default Experience;
+export default Experience
