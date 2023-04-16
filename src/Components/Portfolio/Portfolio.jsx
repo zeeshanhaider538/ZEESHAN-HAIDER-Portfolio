ee0dc825575ad0c6578d4b0983d295f8d7628c4c
Zeeshan Haider <zeeshanhaider538@gmail.com>
2023-04-16 13:46:24 +0500

    Portfolio Section

---
+++ src/Components/Portfolio/Portfolio.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./Portfolio.css"
+import {Swiper, SwiperSlide} from "swiper/react"
+import Sidebar from "../../img/sidebar.png";
+import Ecommerce from "../../img/ecommerce.png";
+import HOC from "../../img/hoc.png";
+import MusicApp from "../../img/musicapp.png";
+import Fitnfit from "../../img/Fitnfit.png";
+import 'swiper/css';
+const Portfolio = () => {
+  return (
+    <div className="portfolio">
+        {/* Heading  */}
+        <span>Recent Projects</span>
+        <span>Portfolio</span>
+
+        {/*  Slider  */}
+        <Swiper>
+<SwiperSlide>
+    <img src={Sidebar} alt='Sidebar'></img>
+</SwiperSlide>
+<SwiperSlide>
+    <img src={Ecommerce} alt='Ecommercer'></img>
+</SwiperSlide>
+<SwiperSlide>
+    <img src={HOC} alt='HOC'></img>
+</SwiperSlide>
+<SwiperSlide>
+    <img src={Fitnfit} alt='{Fitnfit'></img>
+</SwiperSlide>
+        </Swiper>
+        </div>
+  )
+}
+
+export default Portfolio
